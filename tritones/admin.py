30fee361586ee9cf8a0e8b24d3a98c0323ed3105
nickcamp13 <nickreyescampos@gmail.com>
2023-09-10 22:11:09 -0700

    created tritone spotify track model

--- tritones/admin.py
+++ tritones/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import TritoneSpotifyTrack
 
 # Register your models here.
+admin.site.register(TritoneSpotifyTrack)
--- tritones/models.py
+++ tritones/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class TritoneSpotifyTrack(models.Model):
+    name = models.CharField(max_length=255)
+    artist = models.CharField(max_length=255)
+    album = models.CharField(max_length=255)
+    year_released = models.CharField(max_length=255)
+    spotify_id = models.CharField(max_length=255, unique=True)
+    
+    def __str__(self):
+        return self.name
